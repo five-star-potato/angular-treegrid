34874bcb50ba3ef5632e1457af8e10db065d7faf
Cecil Lew <cecil_lew@hotmail.com>
2016-08-22 14:11:58 -0400

    switched to protractor 3. have a working test statement in treegrid-spec.js

--- e2e/treegrid-spec.js
+++ e2e/treegrid-spec.js
@@ -2,7 +2,14 @@ describe('angular 2 treegrid', function() {
   it('should display a treegrid', function() {
     browser.get('http://localhost:3000/home');
 
-    var tg = element(by.tagName('tg-treegrid'));
-    expect(tg.isPresent()).toBe(true);
+    expect(element.all(by.css('.treegrid-table > thead > tr > th')).first().getText()).toMatch(/Emplyee/);
+    /*
+    element.all(by.css('.treegrid-table > thead > tr > th')).then(function (elements) {
+        elements[0].getWebElement().then(function(item) {
+            console.log(item);
+        });
+        expect(elements[0].isPresent()).toBe(true);
+    });
+    */
   });
 });
